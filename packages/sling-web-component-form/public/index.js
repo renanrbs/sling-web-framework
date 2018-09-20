a425e95c13cd9dc25dbfb7fceda7e5ebb8eb23dc
Caio Matheus Veloso Dias <cmdias@stone.com.br>
2018-09-20 18:13:01 -0300

    :poop: Add form component

---
+++ packages/sling-web-component-form/public/index.js
@@ -0,0 +1,21 @@
+import {
+  isNotEmpty,
+  isValidEmail,
+  isValidPhone,
+  isValidCPF,
+  isValidCNPJ,
+} from 'sling-helpers';
+
+const $form = document.querySelector('sling-form');
+
+$form.validation = [
+  isNotEmpty('name'),
+  isNotEmpty('email'),
+  isValidEmail('email'),
+  isNotEmpty('phone'),
+  isValidPhone('phone'),
+  isNotEmpty('cpf'),
+  isValidCPF('cpf'),
+  isNotEmpty('cnpj'),
+  isValidCNPJ('cnpj'),
+];
